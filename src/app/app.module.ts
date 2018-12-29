f78b61acf39040dcce8c951a962cd95e63720e4c
Alberto Seoane <33812609+alchexmist@users.noreply.github.com>
2018-12-29 19:07:11 +0100

    ng generate component init System entry point.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginModule } from './login/login.module';
 import { RegisterModule } from './register/register.module';
+import { InitComponent } from './init/init.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitComponent
   ],
   imports: [
     BrowserModule,
