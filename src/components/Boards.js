7d2f0eb244bd644b9d25038bf2aadb9e20503efb
Alberto Seoane <33812609+alchexmist@users.noreply.github.com>
2019-08-19 19:01:56 +0200

    Corregido mecanismo de "polling" para pizarras. Se aplica "poll" sobre redux store, en lugar de directamente sobre el renderizado de la interfaz. Esto permite corregir efectos visuales no deseados.

--- src/components/Boards.js
+++ src/components/Boards.js
@@ -41,6 +41,12 @@ class Boards extends Component {
     this.props.setUserAction({userActionName: ACTION_EDIT_BOARD, actionEntityID: boardID});
   }
 
+  parseBoardData(boardData) {
+    return (
+      <Card as="a" key={boardData.id} onClick={(e) => this.handleBoardCardClick(boardData.id, e)} style={{cursor: 'pointer'}}><Card.Body><Card.Title>{boardData.title}</Card.Title><Card.Text>{boardData.text}</Card.Text></Card.Body></Card>
+    );
+  }
+
   render() {
     if (this.props.userAction === ACTION_EDIT_BOARD) {
       return <Redirect to={'/edit/board/' + this.props.userActionEntityID} />;
@@ -48,45 +54,80 @@ class Boards extends Component {
 
     return (
       <Container className="mx-auto my-3">
+        <Query query={GET_BOARDS}
+          fetchPolicy={'cache-and-network'}
+          pollInterval={5000}
+          onCompleted={({me}) => {
+            if (!(me.ownerBoards.every((e) => this.props.boards.includes(e)))) {
+              console.log('OWNER BOARDS RECEIVED: ', me.ownerBoards);
+              console.log('OWNER BOARDS STATE: ', this.props.boards);
+              this.props.boardListRequest(me.ownerBoards);
+            }
+            if (!(me.contributorBoards.every((e) => this.props.boards.includes(e)))) {
+              console.log('CONTRIBUTOR BOARDS RECEIVED: ', me.contributorBoards);
+              console.log('CONTRIBUTOR BOARDS STATE: ', this.props.boards);
+              this.props.boardListRequest(me.contributorBoards);
+            }
+          }}
+        >
+          {({loading, error, data}) => {
+            if (loading) return null;
+            if (error) return null;
+
+            return null;
+          }}
+        </Query>
         <CardColumns>
-          <Query query={GET_BOARDS}
-            fetchPolicy={'cache-and-network'}
-            onCompleted={({me}) => {
-              if (!(me.ownerBoards.every((e) => this.props.boards.includes(e)))) {
-                console.log('OWNER BOARDS RECEIVED: ', me.ownerBoards);
-                console.log('OWNER BOARDS STATE: ', this.props.boards);
-                this.props.boardListRequest(me.ownerBoards);
-              }
-              if (!(me.contributorBoards.every((e) => this.props.boards.includes(e)))) {
-                console.log('CONTRIBUTOR BOARDS RECEIVED: ', me.contributorBoards);
-                console.log('CONTRIBUTOR BOARDS STATE: ', this.props.boards);
-                this.props.boardListRequest(me.contributorBoards);
-              }
-            }}
-          >
-            {({loading, error, data}) => {
-              if (loading) return 'Loading...';
-              if (error) return `Error! ${error.message}`;
-
-              // eslint-disable-next-line react/display-name
-              const parseBoardData = (boardData) =>
-                <Card as="a" key={boardData.id} onClick={(e) => this.handleBoardCardClick(boardData.id, e)} style={{cursor: 'pointer'}}><Card.Body><Card.Title>{boardData.title}</Card.Title><Card.Text>{boardData.text}</Card.Text></Card.Body></Card>;
-
-              return (
-                <ul>
-                  {
-                    data.me.ownerBoards.map(parseBoardData)
-                  }
-                  {
-                    data.me.contributorBoards.map(parseBoardData)
-                  }
-                </ul>
-              );
-            }}
-          </Query>
+          <ul>
+            {
+              this.props.boards.map((e) => this.parseBoardData(e))
+            }
+          </ul>
         </CardColumns>
       </Container>
     );
+
+    // return (
+    //   <Container className="mx-auto my-3">
+    //     <CardColumns>
+    //       <Query query={GET_BOARDS}
+    //         fetchPolicy={'cache-and-network'}
+    //         onCompleted={({me}) => {
+    //           if (!(me.ownerBoards.every((e) => this.props.boards.includes(e)))) {
+    //             console.log('OWNER BOARDS RECEIVED: ', me.ownerBoards);
+    //             console.log('OWNER BOARDS STATE: ', this.props.boards);
+    //             this.props.boardListRequest(me.ownerBoards);
+    //           }
+    //           if (!(me.contributorBoards.every((e) => this.props.boards.includes(e)))) {
+    //             console.log('CONTRIBUTOR BOARDS RECEIVED: ', me.contributorBoards);
+    //             console.log('CONTRIBUTOR BOARDS STATE: ', this.props.boards);
+    //             this.props.boardListRequest(me.contributorBoards);
+    //           }
+    //         }}
+    //       >
+    //         {({loading, error, data}) => {
+    //           if (loading) return 'Loading...';
+    //           if (error) return `Error! ${error.message}`;
+
+    //           // eslint-disable-next-line react/display-name
+    //           const parseBoardData = (boardData) =>
+    //             <Card as="a" key={boardData.id} onClick={(e) => this.handleBoardCardClick(boardData.id, e)} style={{cursor: 'pointer'}}><Card.Body><Card.Title>{boardData.title}</Card.Title><Card.Text>{boardData.text}</Card.Text></Card.Body></Card>;
+
+    //           return (
+    //             <ul>
+    //               {
+    //                 data.me.ownerBoards.map(parseBoardData)
+    //               }
+    //               {
+    //                 data.me.contributorBoards.map(parseBoardData)
+    //               }
+    //             </ul>
+    //           );
+    //         }}
+    //       </Query>
+    //     </CardColumns>
+    //   </Container>
+    // );
   }
 }
 
